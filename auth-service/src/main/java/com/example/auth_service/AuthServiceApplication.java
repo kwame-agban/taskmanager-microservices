772f4b9d0756d2892dad7b39e0a29eb845d72b42
Kwame AGBAN <kwame.agban@yahoo.com>
2026-07-03 13:25:25 +0200

    Publier la video de presentation Microservices et Angular appel des Apis

--- auth-service/src/main/java/com/example/auth_service/AuthServiceApplication.java
+++ auth-service/src/main/java/com/example/auth_service/AuthServiceApplication.java
@@ -2,12 +2,14 @@ package com.example.auth_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
-
+    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    System.out.println("== Encode(password) = " + encoder.encode("password"));
     SpringApplication.run(AuthServiceApplication.class, args);
 	}
 
